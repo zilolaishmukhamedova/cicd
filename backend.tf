3880475342e14375d3af0e0ec4ba164580c5c20b
Zilola Ishmukhamedova <khayotishmukhamedov@zilola>
2022-06-07 03:13:10 -0400

    Hoping modified last time

--- backend.tf
+++ backend.tf
@@ -1,5 +1,6 @@
 terraform {
-  backend = "remote" 
-      hostname = "app.terraform.io"
-      organization = "Zilola"
+  backend "remote" {    
+    hostname = "app.terraform.io"
+    organization = "Zilola"
   }
+}
