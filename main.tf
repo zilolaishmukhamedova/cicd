715d14395473f733d7906fd8c4e8b1f2314b5957
Zilola Ishmukhamedova <khayotishmukhamedov@zilola>
2022-06-10 00:45:44 -0400

    Added new Jenkins user

--- main.tf
+++ main.tf
@@ -5,7 +5,9 @@ resource "aws_iam_user" "user1" {
 resource "aws_iam_user" "user2" {
   name = "Tom"
 }
-
+resource "aws_iam_user" "user3" {
+  name = "Jenkins"
+}
 resource "aws_iam_group" "group1" {
   name = "operations"
 }
